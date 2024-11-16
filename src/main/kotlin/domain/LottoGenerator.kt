cdc293ff3b614af59569eec93eb33129f43565d9
MyeongSooJang <mmsgod0427@naver.com>
2024-11-16 18:10:31 +0900

    refactor: LottoGenerator 상수 접근제어자 public으로 변경

--- src/main/kotlin/domain/LottoGenerator.kt
+++ src/main/kotlin/domain/LottoGenerator.kt
@@ -3,8 +3,8 @@ package domain
 class LottoGenerator {
 
     companion object {
-        private val LOTTO_NUMBER_RANGE = 1..45
-        private val LOTTO_NUMBER_COUNT = 6
+        val LOTTO_NUMBER_RANGE = 1..45
+        val LOTTO_NUMBER_COUNT = 6
 
         fun generate() : List<Int>{
             return LOTTO_NUMBER_RANGE.shuffled().take(LOTTO_NUMBER_COUNT)

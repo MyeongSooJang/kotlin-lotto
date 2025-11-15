94faa3ecbc60602375bc811ee7b354597baff8ee
MyeongSooJang <mmsgod0427@naver.com>
2025-11-15 20:48:47 +0900

    refactor: LottoBundle 메소드 분리

--- src/main/kotlin/domain/LottoBundle.kt
+++ src/main/kotlin/domain/LottoBundle.kt
@@ -5,18 +5,18 @@ import domain.Rank.Companion.findRank
 class LottoBundle(val lottos: List<Lotto>) {
 
     fun checkRanks(winningNumbers: Lotto, bonusNumber: Int): Map<Rank, Int> {
-        val rankCounts = mutableMapOf<Rank, Int>()
-        for (lotto in lottos) {
-            val matchCount = lotto.matchCount(winningNumbers)
-            val matchBonus = if (matchCount == 5) {
-                lotto.containsBonusNumber(bonusNumber)
-            } else {
-                false
-            }
-            val rank = findRank(matchCount, matchBonus)
-            rankCounts[rank] = rankCounts.getOrDefault(rank, 0) + 1
-        }
-        return rankCounts
+
+        return lottos.map{ lotto -> calculateRank(lotto, winningNumbers,bonusNumber) }
+            .groupingBy { it }
+            .eachCount()
+    }
+
+    private fun calculateRank(lotto: Lotto,
+                              winningNumbers: Lotto,
+                              bonusNumber: Int): Rank {
+        val matchCount = lotto.matchCount(winningNumbers)
+        val matchBonus = matchCount == 5 && lotto.containsBonusNumber(bonusNumber)
+        return findRank(matchCount, matchBonus)
     }
 
 

ccec66ce2d4436be25a15af383d3e9d6183ee163
MyeongSooJang <mmsgod0427@naver.com>
2025-11-15 20:17:30 +0900

    feat: LottoBundle 도메인 구현

--- src/main/kotlin/domain/LottoBundle.kt
+++ src/main/kotlin/domain/LottoBundle.kt
@@ -1,4 +1,23 @@
 package domain
 
-class LottoBundle {
+import domain.Rank.Companion.findRank
+
+class LottoBundle(val lottos: List<Lotto>) {
+
+    fun checkRanks(winningNumbers: Lotto, bonusNumber: Int): Map<Rank, Int> {
+        val rankCounts = mutableMapOf<Rank, Int>()
+        for (lotto in lottos) {
+            val matchCount = lotto.matchCount(winningNumbers)
+            val matchBonus = if (matchCount == 5) {
+                lotto.containsBonusNumber(bonusNumber)
+            } else {
+                false
+            }
+            val rank = findRank(matchCount, matchBonus)
+            rankCounts[rank] = rankCounts.getOrDefault(rank, 0) + 1
+        }
+        return rankCounts
+    }
+
+
 }

30160f92c6aa39586dbe3841970f5b555ff41754
MyeongSooJang <mmsgod0427@naver.com>
2025-11-17 20:31:47 +0900

    refactor:  Lotto를 단순한 일급 컬렉션으로 변경

--- src/main/kotlin/domain/Lotto.kt
+++ src/main/kotlin/domain/Lotto.kt
@@ -1,34 +1,22 @@
 package domain
 
-class Lotto(numbers : List<Int>) {
-    val lottoNumbers : List<Int>
-
-    fun matchCount(winningNumbers : Lotto) : Int {
-        return lottoNumbers.count { it in winningNumbers.lottoNumbers }
-    }
-
-    fun containsBonusNumber(number : Int) : Boolean {
-        return lottoNumbers.contains(number)
-    }
+class Lotto(numbers: List<Int>) {
+    val lottoNumbers: List<Int> = numbers.sorted()
 
     init {
-        require(numbers.size == LOTTO_COUNT) {
-            "로또 번호는 ${LOTTO_COUNT}개여야 합니다."
+        require(numbers.size == LottoConstant.LOTTO_COUNT) {
+            "로또 번호는 ${LottoConstant.LOTTO_COUNT}개여야 합니다."
         }
-        require(numbers.all { it in NUMBER_RANGE }) {
-            "로또 번호는 ${NUMBER_RANGE.first} 부터 ${NUMBER_RANGE.last}이여야 합니다."
+        require(numbers.all { it in LottoConstant.LOTTO_RANGE }) {
+            "로또 번호는 ${LottoConstant.LOTTO_RANGE.first} 부터 ${LottoConstant.LOTTO_RANGE.last}이여야 합니다."
         }
         require(numbers.distinct().size == numbers.size) {
             "로또 번호는 중복이 불가능 합니다."
         }
 
-        this.lottoNumbers = numbers.sorted()
+
     }
 
-    companion object {
-        private val NUMBER_RANGE = 1..45
-        private const val LOTTO_COUNT = 6
-    }
 
 }
 

66caa0133146316a89e5374accf73aff206ea4cb
MyeongSooJang <mmsgod0427@naver.com>
2025-11-17 22:02:25 +0900

    feat: LottoApplication 파일 생성

--- src/main/kotlin/controller/LottoApplication.kt
+++ src/main/kotlin/controller/LottoApplication.kt
@@ -1,4 +1,13 @@
 package controller
 
-class LottoApplication {
+import LottoMachine
+import view.InputView
+import view.OutputView
+
+fun main() {
+    val inputView = InputView()
+    val outputView = OutputView()
+    val lottoMachine = LottoMachine(inputView, outputView)
+
+    lottoMachine.run()
 }

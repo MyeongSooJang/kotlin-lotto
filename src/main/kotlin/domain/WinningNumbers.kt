f86eac80717ffe40faf225d57aede61642851b10
MyeongSooJang <mmsgod0427@naver.com>
2025-11-17 20:08:27 +0900

    feat:  WinningNumbers 도메인 구현

---
+++ src/main/kotlin/domain/WinningNumbers.kt
@@ -0,0 +1,40 @@
+package domain
+
+class WinningNumbers(val numbers : List<Int>, val bonusNumber : Int) {
+
+    init{
+        require(numbers.size == LottoConstant.LOTTO_COUNT) {
+            "당첨 번호는 ${LottoConstant.LOTTO_COUNT}개여야 합니다."
+        }
+        require(numbers.all { it in LottoConstant.LOTTO_RANGE }) {
+            "당첨 번호는 ${LottoConstant.LOTTO_RANGE.first}부터 ${LottoConstant.LOTTO_RANGE.last}까지여야 합니다."
+        }
+        require(numbers.distinct().size == numbers.size) {
+            "당첨 번호는 중복될 수 없습니다."
+        }
+        require(bonusNumber in LottoConstant.LOTTO_RANGE) {
+            "보너스 번호는 ${LottoConstant.LOTTO_RANGE.first}부터 ${LottoConstant.LOTTO_RANGE.last}까지여야 합니다."
+        }
+        require(bonusNumber !in numbers) {
+            "보너스 번호는 당첨 번호와 중복될 수 없습니다."
+        }
+    }
+
+    fun match(lotto : Lotto) : Rank {
+        val matchCount = numbers.count { it in lotto.lottoNumbers }
+        val hasBonus = lotto.lottoNumbers.contains(bonusNumber)
+        return calculateRank(matchCount, hasBonus)
+    }
+
+    private fun calculateRank(matchCount : Int, hasBonus : Boolean) : Rank {
+        return when (matchCount) {
+            6 -> Rank.FIRST
+            5 if hasBonus -> Rank.SECOND
+            5 -> Rank.THIRD
+            4 -> Rank.FORTH
+            3 -> Rank.FIFTH
+            else -> Rank.NONE
+        }
+    }
+
+}

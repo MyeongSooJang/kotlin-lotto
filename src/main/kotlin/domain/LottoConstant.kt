1cf84d11b5ad1ddfb0c73550467d9a50d1eaf506
MyeongSooJang <mmsgod0427@naver.com>
2025-11-17 21:10:09 +0900

    feat: 프로퍼티 추가

--- src/main/kotlin/domain/LottoConstant.kt
+++ src/main/kotlin/domain/LottoConstant.kt
@@ -1,8 +1,8 @@
 package domain
 
 object LottoConstant {
-    const val MIN_NUMBER = 1
-    const val MAX_NUMBER = 45
     const val LOTTO_COUNT = 6
     val LOTTO_RANGE = 1..45
+    const val LOTTO_PRICE = 1000
+    val PURCHASE_COUNT_RANGE = 1..100
 }

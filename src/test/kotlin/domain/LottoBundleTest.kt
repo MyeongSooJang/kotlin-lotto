1cdcd67d8adb1d264e1bae6241cb974e83159d77
MyeongSooJang <mmsgod0427@naver.com>
2025-11-21 15:40:40 +0900

    test: LottoBundle 테스트 개선
      - 의미없는 assertion 제거 및 검증 로직 추가
      - getLotto 메소드 테스트 추가
      - 빈 묶음 테스트 추가
      - 100장 대량 처리 테스트 추가
      - 모든 등수 혼합 시나리오 추가

--- src/test/kotlin/domain/LottoBundleTest.kt
+++ src/test/kotlin/domain/LottoBundleTest.kt
@@ -1,8 +1,6 @@
 package domain
 
-import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.DescribeSpec
-import io.kotest.matchers.collections.shouldContainExactly
 import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.shouldBe
 
@@ -18,9 +16,46 @@ class LottoBundleTest : DescribeSpec({
 
             it("여러 로또들의 모음으로 생성이 된다") {
                 val lottoBundle = LottoBundle(listOf(lotto1, lotto2, lotto3, lotto4, lotto5))
+
+                lottoBundle.getLotto() shouldHaveSize 5
+                lottoBundle.getLotto() shouldBe listOf(lotto1, lotto2, lotto3, lotto4, lotto5)
             }
+
             it("한 개인 경우에도 생성이 가능하다") {
                 val lottoBundle = LottoBundle(listOf(lotto1))
+
+                lottoBundle.getLotto() shouldHaveSize 1
+                lottoBundle.getLotto()[0] shouldBe lotto1
+            }
+
+            it("빈 묶음도 생성 가능하다") {
+                val lottoBundle = LottoBundle(emptyList())
+
+                lottoBundle.getLotto() shouldHaveSize 0
+                lottoBundle.getLotto() shouldBe emptyList()
+            }
+        }
+
+        context("getLotto 메소드") {
+
+            it("저장된 모든 로또를 반환한다") {
+                val lotto1 = Lotto(listOf(1, 2, 3, 4, 5, 6))
+                val lotto2 = Lotto(listOf(7, 8, 9, 10, 11, 12))
+                val lottoBundle = LottoBundle(listOf(lotto1, lotto2))
+
+                val result = lottoBundle.getLotto()
+
+                result shouldHaveSize 2
+                result[0] shouldBe lotto1
+                result[1] shouldBe lotto2
+            }
+
+            it("빈 묶음은 빈 리스트를 반환한다") {
+                val lottoBundle = LottoBundle(emptyList())
+
+                val result = lottoBundle.getLotto()
+
+                result shouldBe emptyList()
             }
         }
     }
@@ -54,6 +89,53 @@ class LottoBundleTest : DescribeSpec({
                 val winningNumber = WinningNumbers(listOf(11, 12, 13, 14, 15, 19), 8)
                 val result = lottoBundle.checkRanks(winningNumber)
                 result[Rank.NONE] shouldBe 4
+            }
+        }
+
+        context("대량 로또 처리") {
+
+            it("100장 전체 낙첨 시나리오") {
+                val lottos = List(100) { Lotto(listOf(10, 11, 12, 13, 14, 15)) }
+                val lottoBundle = LottoBundle(lottos)
+                val winningNumbers = WinningNumbers(listOf(1, 2, 3, 4, 5, 6), 7)
+
+                val result = lottoBundle.checkRanks(winningNumbers)
+
+                result[Rank.NONE] shouldBe 100
+                result.keys.filter { it != Rank.NONE } shouldHaveSize 0
+            }
+
+            it("빈 LottoBundle은 빈 Map을 반환한다") {
+                val lottoBundle = LottoBundle(emptyList())
+                val winningNumbers = WinningNumbers(listOf(1, 2, 3, 4, 5, 6), 7)
+
+                val result = lottoBundle.checkRanks(winningNumbers)
+
+                result shouldBe emptyMap()
+            }
+        }
+
+        context("다양한 등수 혼합") {
+
+            it("모든 등수가 골고루 포함된 경우") {
+                val lotto1 = Lotto(listOf(1, 2, 3, 4, 5, 6))
+                val lotto2 = Lotto(listOf(1, 2, 3, 4, 5, 7))
+                val lotto3 = Lotto(listOf(1, 2, 3, 4, 5, 10))
+                val lotto4 = Lotto(listOf(1, 2, 3, 4, 10, 11))
+                val lotto5 = Lotto(listOf(1, 2, 3, 10, 11, 12))
+                val lottofail = Lotto(listOf(20, 21, 22, 23, 24, 25))
+
+                val lottoBundle = LottoBundle(listOf(lotto1, lotto2, lotto3, lotto4, lotto5, lottofail))
+                val winningNumbers = WinningNumbers(listOf(1, 2, 3, 4, 5, 6), 7)
+
+                val result = lottoBundle.checkRanks(winningNumbers)
+
+                result[Rank.FIRST] shouldBe 1
+                result[Rank.SECOND] shouldBe 1
+                result[Rank.THIRD] shouldBe 1
+                result[Rank.FOURTH] shouldBe 1
+                result[Rank.FIFTH] shouldBe 1
+                result[Rank.NONE] shouldBe 1
             }
         }
     }
